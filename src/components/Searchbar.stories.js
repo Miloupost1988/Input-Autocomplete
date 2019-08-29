e9310d82753015bfd219038f55a866bbc7e43489
Milou Post <post.milou@gmail.com>
2019-08-29 14:00:10 +0200

    Revert "add stories for searchbar container and searchsuggestions"
    This reverts commit 987e94ff3410935c4f35b3745a8f4c91a7eb9203.

---
+++ src/components/Searchbar.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import SearchBar from './SearchBar';
+
+storiesOf('SearchBar', module)
+  .add('default with hardcoded text', () => <SearchBar/>)
+  .add('searchbar with dynamic text', () => <SearchBar
+    placeholder='Zoeken'
+    errorMessage="oeps, er is iets misgegaan"
+  />);
